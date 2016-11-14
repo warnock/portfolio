d10f158d2c9f1f65dc88d7bf2f05f1395015e25f
Megan Warnock <meganwarnock@Megans-MacBook-Air.local>
2016-11-13 19:11:10 -0800

    pushing first project to github

---
+++ JavaScript/portfolio.js
@@ -0,0 +1,28 @@
+
+$('.mainArrowButton').click(function() {
+  $('html, body').animate({
+    scrollTop: $('#navContainer').offset().top}, 'slow');
+
+  })
+
+$('.home').click(function() {
+  $('html, body').animate({
+    scrollTop: $('html').offset().top}, 'slow');
+
+  })
+
+$('.about').click(function() {
+  $('html, body').animate({
+    scrollTop: $('#navContainer').offset().top}, 'slow');
+
+  })
+
+$('.work').click(function() {
+  $('html, body').animate({
+    scrollTop: $('#workAnchor').offset().top}, 'slow');
+  })
+
+  $('.contact').click(function() {
+    $('html, body').animate({
+      scrollTop: $('#contact').offset().top}, 'slow');
+    })
